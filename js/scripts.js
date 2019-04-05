869354c5c0d50d5c42bed594a3e66b01c1f2e8b9
Kyle Harrington <harrington.kyle@gmail.com>
2019-04-05 13:08:44 -0700

    add for loop to push all digits into one string

--- js/scripts.js
+++ js/scripts.js
@@ -13,58 +13,29 @@ $(document).ready(function() {
     console.log(result, 'this is result')
     $('#result').text(result);
 
-
-
   });
 
     function toMessage(num) {
       console.log(num, 'num')
-
       // return invalid number if number is below 1
       if (isNaN(num) || num < 0) {
         return 'please enter a valid number'
       }
 
-//// if the number has 1 2 or 3 in it
-      if (num.includes(3)) {
-        return dave;
+      var message = [];
+
+      // while i is less than the num keep iterating through and push proper iteration
+      for (i=0; i <= num; i++) {
+        if (String(i).includes(3)) {
+          message.push(dave);
+        } else if (String(i).includes(2)) {
+          message.push(boop);
+        } else if (String(i).includes(1)){
+          message.push(beep)
+        } else {
+          message.push(i)
+        }
       }
-      if (num.includes(2)) {
-        return boop;
-      }
-      if (num.includes(1)) {
-        return beep;
-      }
+      return message.join(", ");
     }
-
-///
-      // if (num === 3) {
-      //   return dave;
-      // } else if (num === 2) {
-      //   return boop;
-      // } else if (num === 1) {
-      //   return beep;
-      // }
-      //
-
-
-
-
-
-
-        // var messageSet = {
-        //   3: "Beep!",
-        //   2: "Boop!",
-        //   1: "I'm Sorry Dave. I can't do that."
-        // };
-        // var message = "";
-        //
-        // for (var key in messageSet) {
-        //   while (num >= messageSet[key]) {
-        //     message += key;
-        //     num -= messageSet[key];
-        //   }
-        // }
-        // return message;
-        // }
-    });
+  });
