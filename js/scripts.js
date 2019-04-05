646750003b4d3b318817dc4ed2a0184f30eb890f
Kyle Harrington <harrington.kyle@gmail.com>
2019-04-05 12:02:55 -0700

    add if statements for num.includes(3/2/1)

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,70 @@
 $(document).ready(function() {
-  $('form#roman').submit(function(event) {
+  var beep = "Beep!";
+  var boop = "Boop!";
+  var dave = "I'm sorry Dave. I can't do that.";
+  var x = dave.includes(3)
+
+  $('form#beep').submit(function(event) {
     event.preventDefault();
 
-    var num = parseInt($('input#number').val());
-    var result = toBeep(num);
+    var num = $('input#number').val();
+    console.log(num, 'line10')
+    var result = toMessage(num);
+    console.log(result, 'this is result')
     $('#result').text(result);
 
-    function toBeep(num) {
-      if (isNaN(num) || num < 0 ||) {
+
+
+  });
+
+    function toMessage(num) {
+      console.log(num, 'num')
+
+      // return invalid number if number is below 1
+      if (isNaN(num) || num < 0) {
         return 'please enter a valid number'
       }
+
+//// if the number has 1 2 or 3 in it
+      if (num.includes(3)) {
+        return dave;
+      }
+      if (num.includes(2)) {
+        return boop;
+      }
+      if (num.includes(1)) {
+        return beep;
+      }
     }
+
+///
+      // if (num === 3) {
+      //   return dave;
+      // } else if (num === 2) {
+      //   return boop;
+      // } else if (num === 1) {
+      //   return beep;
+      // }
+      //
+
+
+
+
+
+
+        // var messageSet = {
+        //   3: "Beep!",
+        //   2: "Boop!",
+        //   1: "I'm Sorry Dave. I can't do that."
+        // };
+        // var message = "";
+        //
+        // for (var key in messageSet) {
+        //   while (num >= messageSet[key]) {
+        //     message += key;
+        //     num -= messageSet[key];
+        //   }
+        // }
+        // return message;
+        // }
+    });
