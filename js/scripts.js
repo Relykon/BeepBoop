cb715bb0032208b0aabf91d6152e6decac176617
Kyle Harrington <harrington.kyle@gmail.com>
2019-04-08 20:42:53 -0700

    seperate business and UI logic, remove console logs, add missing semi-colons

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,34 @@
 $(document).ready(function() {
-  var beep = "Beep!";
-  var boop = "Boop!";
-  var dave = "I'm sorry Dave. I'm afraid can't do that.";
-
   $('form#beep').submit(function(event) {
     event.preventDefault();
     $("#Hal").show();
-    
-
 
     var num = $('input#number').val();
-    console.log(num, 'line10')
     var result = toMessage(num);
-    console.log(result, 'this is result')
     $('#result').text(result);
-
   });
+});
 
-    function toMessage(num) {
-      console.log(num, 'num')
-      // return invalid number if number is below 1
-      if (isNaN(num) || num < 0) {
-        return 'please enter a valid number'
-      }
+function toMessage(num) {
+  var beep = "Beep!";
+  var boop = "Boop!";
+  var dave = "I'm sorry Dave. I'm afraid can't do that.";
+  var message = [];
 
-      var message = [];
+  if (isNaN(num) || num < 0) {
+    return 'please enter a valid number';
+  }
 
-      // while i is less than the num keep iterating through and push proper iteration
-      for (i=0; i <= num; i++) {
-        if (String(i).includes(3)) {
-          message.push(dave);
-        } else if (String(i).includes(2)) {
-          message.push(boop);
-        } else if (String(i).includes(1)){
-          message.push(beep)
-        } else {
-          message.push(i)
-        }
-      }
-      return message.join(", ");
+  for (i=0; i <= num; i++) {
+    if (String(i).includes(3)) {
+      message.push(dave);
+    } else if (String(i).includes(2)) {
+      message.push(boop);
+    } else if (String(i).includes(1)) {
+      message.push(beep);
+    } else {
+      message.push(i);
     }
-  });
+  }
+  return message.join(", ");
+}
